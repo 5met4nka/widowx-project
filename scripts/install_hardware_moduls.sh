ec871590af80856cbc682ab40f21e62babd688b6
5met4nka <nlobazev@gmail.com>
2024-08-21 21:11:44 +0300

    rules is work

--- scripts/install_hardware_moduls.sh
+++ scripts/install_hardware_moduls.sh
@@ -11,7 +11,7 @@ rules_file="/etc/udev/rules.d/widowx.rules"
 if [ -f "$rules_file" ]; then
     echo "The WIDOWX device is already installed"
 else
-    echo 'SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", ATTRS{serial}=="AR0JW9R1", MODE:="0666", GROUP:="dialout", SYMLINK+="ttyUSB_WIDOWX"' > /etc/udev/rules.d/widowx.rules
+    echo 'SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", ATTRS{serial}=="AR0JW9R1", MODE:="0666", GROUP:="dialout", SYMLINK+="ttyWIDOWX"' > /etc/udev/rules.d/widowx.rules
 
     service udev reload
     sleep 2
